f6b605f2356ad48c05ae6bebf20d77190338d06e
Maria Mercedes Chomicki <mmchomicki1995@gmail.com>
2023-10-09 23:51:02 -0300

    Validaciones en form y aviso registro exitoso

--- JS/login-signup.js
+++ JS/login-signup.js
@@ -12,87 +12,27 @@ document.getElementById("logIn").addEventListener('click', (e)=>{
 
 //Validations
 
- const form = document.querySelector(".form");
- const inputs = document.querySelectorAll(".form input");
-
- let btnSubmit = document.querySelectorAll(".btn-submit");
-
-function redirection(){
-    location.href = "home.html";
-    form.reset();
-}
-
-btnSubmit.forEach((btn)=>{
-    btn.addEventListener('click', redirection);
-})
+const form = document.querySelector(".form");
+const inputs = document.querySelectorAll(".form input");
 
 const expressions = {
-   password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,12}$/   //la cual valida contraseñas de al menos una letra, al menos un numero, al menos una letra mayúscula, al menos 8 caracteres, no permite espacios.   
+   password1: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,12}$/
 }
 
 const fields = {
-	password: false
+	password1: false,
+    password2: false
 }
 
 const validateForm = (e)=>{
     switch(e.target.name){
-        case "password":
-            validateField(expressions.password, e.target, 'password');
-			validatePassword2();
+        case "password1":
+            validateField(expressions.password1, e.target, 'password1');
         break;
         case "password2":
-            validatePassword2();
+            validatePassword2('password2');
         break;
     }
-}
-
-const validateField = (expression, input, field) => {
-	if(expression.test(input.value)){
-		document.getElementById(`${field}-group`).classList.remove('form-group-incorrect');
-		document.getElementById(`${field}-group`).classList.add('form-group-correct');
-		document.querySelector(`#${field}-group i`).classList.add('fas');
-        document.querySelector(`#${field}-group i`).classList.add('fa-check-circle');
-		document.querySelector(`#${field}-group i`).classList.remove('fa-solid');
-        document.querySelector(`#${field}-group i`).classList.remove('fa-circle-exclamation');
-        document.querySelector(`#${field}-group`).classList.remove('form-group-error');
-		document.querySelector(`#${field}-group .form-input-error`).classList.remove('form-input-error-active');
-		fields[field] = true;
-	} else {
-		document.getElementById(`${field}-group`).classList.add('form-group-incorrect');
-		document.getElementById(`${field}-group`).classList.remove('form-group-correct');
-		document.querySelector(`#${field}-group i`).classList.add('fa-solid');
-        document.querySelector(`#${field}-group i`).classList.add('fa-circle-exclamation');
-		document.querySelector(`#${field}-group i`).classList.remove('fas');
-        document.querySelector(`#${field}-group i`).classList.remove('fa-check-circle');
-        document.querySelector(`#${field}-group`).classList.add('form-group-error');
-		document.querySelector(`#${field}-group .form-input-error`).classList.add('form-input-error-active');
-		fields[field] = false;
-	}
-}
-
-const validatePassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
-
-	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`password2-group`).classList.add('form-group-incorrect');
-		document.getElementById(`password2-group`).classList.remove('form-group-correct');
-		document.querySelector(`#password2-group i`).classList.add('fa-solid');
-        document.querySelector(`#password2-group i`).classList.add('fa-circle-exclamation');
-		document.querySelector(`#password2-group i`).classList.remove('fas');
-        document.querySelector(`#password2-group i`).classList.remove('fa-check-circle');
-		document.querySelector(`#password2-group .form-input-error`).classList.add('form-input-error-active');
-		fields['password'] = false;
-	} else {
-		document.getElementById(`password2-group`).classList.remove('form-group-incorrect');
-		document.getElementById(`password2-group`).classList.add('form-group-correct');
-        document.querySelector(`#password2-group i`).classList.remove('fa-solid');
-        document.querySelector(`#password2-group i`).classList.remove('fa-circle-exclamation');
-		document.querySelector(`#password2-group i`).classList.add('fas');
-        document.querySelector(`#password2-group i`).classList.add('fa-check-circle');
-		document.querySelector(`#password2-group .form-input-error`).classList.remove('form-input-error-active');
-		fields['password'] = true;
-	}
 }
 
 inputs.forEach((input)=>{
@@ -100,21 +40,79 @@ inputs.forEach((input)=>{
     input.addEventListener('blur', validateForm);
 })
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
+const validateField = (expression, input, field) => {
+    if(expression.test(input.value)){
+        document.getElementById(`${field}-group`).classList.remove('form-group-incorrect');
+        document.getElementById(`${field}-group`).classList.add('form-group-correct');
+        document.querySelector(`#${field}-group i`).classList.add('fas');
+        document.querySelector(`#${field}-group i`).classList.add('fa-check-circle');
+        document.querySelector(`#${field}-group i`).classList.remove('fa-solid');
+        document.querySelector(`#${field}-group i`).classList.remove('fa-circle-exclamation');
+        document.querySelector(`#${field}-group`).classList.remove('form-group-error');
+        document.querySelector(`#${field}-group .form-input-error`).classList.remove('form-input-error-active');
+        fields[field] = true;
+    } else {
+        document.getElementById(`${field}-group`).classList.add('form-group-incorrect');
+        document.getElementById(`${field}-group`).classList.remove('form-group-correct');
+        document.querySelector(`#${field}-group i`).classList.add('fa-solid');
+        document.querySelector(`#${field}-group i`).classList.add('fa-circle-exclamation');
+        document.querySelector(`#${field}-group i`).classList.remove('fas');
+        document.querySelector(`#${field}-group i`).classList.remove('fa-check-circle');
+        document.querySelector(`#${field}-group`).classList.add('form-group-error');
+        document.querySelector(`#${field}-group .form-input-error`).classList.add('form-input-error-active');
+        fields[field] = false;
+    }
+}
+
+const validatePassword2 = (field) => {
+    
+    let inputPassword1 = document.getElementById('password1').value;
+    let inputPassword2 = document.getElementById('password2').value;
+
+    if(inputPassword1 !== inputPassword2){
+        document.getElementById(`password2-group`).classList.add('form-group-incorrect');
+        document.getElementById(`password2-group`).classList.remove('form-group-correct');
+        document.querySelector(`#password2-group i`).classList.add('fa-solid');
+        document.querySelector(`#password2-group i`).classList.add('fa-circle-exclamation');
+        document.querySelector(`#password2-group i`).classList.remove('fas');
+        document.querySelector(`#password2-group i`).classList.remove('fa-check-circle');
+        document.querySelector(`#password2-group .form-input-error`).classList.add('form-input-error-active');
+        fields[field] = false;
+    } else {
+        document.getElementById(`password2-group`).classList.remove('form-group-incorrect');
+        document.getElementById(`password2-group`).classList.add('form-group-correct');
+        document.querySelector(`#password2-group i`).classList.remove('fa-solid');
+        document.querySelector(`#password2-group i`).classList.remove('fa-circle-exclamation');
+        document.querySelector(`#password2-group i`).classList.add('fas');
+        document.querySelector(`#password2-group i`).classList.add('fa-check-circle');
+        document.querySelector(`#password2-group .form-input-error`).classList.remove('form-input-error-active');
+        fields[field] = true;
+    }
+}
+
+function redirection(){
     location.href = "home.html";
     form.reset();
+}
 
-    if(fields.password){
-        let successMsg = document.getElementById('success-msg').classList.add('success-msg-active');
-        
-		setTimeout(() => {
-            successMsg.classList.remove('success-msg-active');
-		}, 5000);
-        
+document.getElementById("registroForm").addEventListener("submit", function(event) {
 
-		document.querySelectorAll('.form-group-correct').forEach((icon) => {
-			icon.classList.remove('form-group-correct');
-		});
-    } 
+    if (fields.password1 && fields.password2) {
+        event.preventDefault();
+        document.getElementById('success-msg').classList.add('success-msg-active');
+        setTimeout(() => {
+            redirection();
+        }, 2000);
+    } else {
+        event.preventDefault();
+    }
+});
+
+document.getElementById("registroLogin").addEventListener("submit", (e)=>{
+    const form = document.querySelector(".form");
+    const inputs = document.querySelectorAll(".form input");
+
+    document.getElementById("btn-submit").addEventListener('click', redirection)
+
 })
+
